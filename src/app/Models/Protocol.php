00c5b829f97e28e2e1ba0f6f92061ea492403e08
Francisco Castelan <francisco.castelan@covaf.com>
2024-02-26 22:12:19 -0600

    Modelo Protocol

---
+++ src/app/Models/Protocol.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Protocol extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'protocol_id',
+        'student_ID',
+        'title_protocol',
+        'staff_ID',
+        'keywords',
+        'protocol_doc',
+    ];
+
+    // Definir la relación con el modelo User
+    public function protocol()
+    {
+        return $this->belongsTo(Protocol::class);
+    }
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::deleting(function ($protocol) {
+            $protocol->user()->delete();
+        });
+    }
+}
