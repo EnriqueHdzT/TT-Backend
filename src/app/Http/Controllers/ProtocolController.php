84ce7394d55a54ece7f689a3ce5effd23498ed25
Kike <ehernandezt1302@alumno.ipn.mx>
2024-03-18 17:05:25 -0600

    Agrega inicios de funcionalidad para subir protocolo

--- src/app/Http/Controllers/ProtocolController.php
+++ src/app/Http/Controllers/ProtocolController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Validator;
+
 
 use App\Models\User;
 use App\Models\Staff;
@@ -17,37 +19,123 @@ class ProtocolController extends Controller
     public function createProtocol(Request $request)
     {
         $request->validate([
-            'title_protocol' => 'required|string',
-            'student_ID' => 'required|string|unique:students,student_ID',
-            'staff_ID' => 'required|string|unique:staff,staff_ID',
-            'keywords' => 'required|string',
-            'protocol_doc' => 'binary',
+            'title' => 'required|string',
+            'students' => 'required|array|max:4',
+            'students.*.email' => 'required|string',
+            'students.*.student_ID' => 'required|string',
+            'staffs' => 'required|array|max:2',
+            'staffs.*.email' => 'required|string',
+            'staffs.*.name' => 'string',
+            'staffs.*.first_lastName' => 'string',
+            'staffs.*.second_lastName' => 'string',
+            'staffs.*.staff_ID' => 'string',
+            'staffs.*.school' => 'string',
+            'staffs.*.academy' => 'string',
+            'staffs.*.pdf' => 'file|mimes:pdf',
+            'keywords' => 'required|array|max:4',
+            'keywords.*.keyword' => 'required|string',
+            'keywords.*.subject' => 'string',
+            'keywords.*.department' => 'string',
+            'keywords.*.academy' => 'string',
+            'pdf' => 'file|mimes:pdf',
         ]);
-    
-    $userId = auth()->id();
-    $protocol = new Protocol();
-    $protocol->user_id = $userId;
-    $protocol->save();
 
+        // Validate and process students
+        $studentIDs = [];
+        $studentEmails = [];
+        foreach ($request->students as $student) {
+            $existingStudent = User::where('email', $student['email'])->first();
+
+            if ($existingStudent) {
+                $student['name'] = $existingStudent->student['name'];
+                $student['first_lastName'] = $existingStudent->student['first_lastName'];
+                $student['second_lastName'] = $existingStudent->student['second_lastName'];
+                $student['student_ID'] = $existingStudent->student['student_ID'];
+                $student['career'] = $existingStudent->student['career'];
+                $student['curriculum'] = $existingStudent->student['curriculum'];
+
+            } else {
+                $studentsValidator = Validator::make($student, [
+                    'email' => 'required|string',
+                    'name' => 'required|string',
+                    'first_lastName' => 'required|string',
+                    'second_lastName' => 'required|string',
+                    'student_ID' => 'required|string',
+                    'career' => 'required|in:ISW,IIA,ICD',
+                    'curriculum' => 'required|date_format:Y',
+                ]);
+                
+                if($studentsValidator->fails()){
+                    $errors = $studentsValidator->errors()->toArray();
+                    return response()->json([$errors], 400);
+                }
+                $newUser = new User();
+                $newUser->email = $student['email'];
+                $newUser->save();
+                
+                $newStudent = new Student();
+                $newStudent->user_id = $newUser->id;
+                $newStudent->name = $student['first_lastName'];
+                $newStudent->lastname = $student['second_lastName'];
+                $newStudent->second_lastname = $student['name'];
+                $newStudent->student_ID = $student['student_ID'];
+                $newStudent->career = $student['career'];
+                $newStudent->curriculum = $student['curriculum'];
+                $newStudent->save();
+            }
+            $studentIDs[] = $student['student_ID'];
+            $studentEmail = $student['email'];
+            $studentEmails[] = $student['email'];
+        }
         
+        // Validate students duplicity
+        $uniqueStudentIDs = array_unique($studentIDs);
+        $uniqueStudentEmails = array_unique($studentEmails);
+        if (count($studentIDs) !== count($uniqueStudentIDs) || count($studentEmails) !== count($uniqueStudentEmails)) {
+            return response()->json(['error' => 'Duplicate student IDs or emails'], 400);
+        }
 
-        return response()->json(['message' => 'Protocolo creado exitosamente'], 201);
+        $staffs = [];
+        $staffIDs = [];
+        $staffEmails = [];
+        $hasESCOM = false;
+        foreach ($request->staffs as $staff) {
+            $staffEmail = $staff['email'];
+            $staffIDs[] = $staff['staff_ID'];
+            $staffEmails[] = $staff['email'];
+            if ($staff['school'] === 'ESCOM') {
+                $hasESCOM = true;
+            }
+            $existingStaff = User::where('email', $staffEmail)->first();
+
+            if ($existingStaff) {
+                $staffs[] = $existingStaff->toArray();
+            } else {
+                $staffs[] = $staff;
+            }
+        }
+
+        // Validate staffs duplicity
+        $uniqueStaffIDs = array_unique($staffIDs);
+        $uniqueStaffEmails = array_unique($staffEmails);
+        if (count($staffIDs) !== count($uniqueStaffIDs) || count($staffEmails) !== count($uniqueStaffEmails)) {
+            return response()->json(['error' => 'Duplicate staff IDs or emails'], 400);
+        }
+        if (!$hasESCOM) {
+            return response()->json(['error' => 'At least one staff member must have school ESCOM'], 400);
+        }
+        return response()->json($request->students, 201);
     }
 
     public function readProtocol($id)
     {
-        $protocol= Protocol::find($id);
+        $protocol = Protocol::find($id);
 
         if (!$protocol) {
             return response()->json(['message' => 'Protocolo no encontrado'], 404);
         }
 
-        $protocolData = $protocol->toArray();
-        unset($protocolData['protocol_id']);
-        unset($protocolData['updated_at']);
-        unset($protocolData['created_at']);
-
-        return response()->json(['protocol' => $protocolData], 200);
+        return response()->json(['protocol' => $protocol], 200);
     }
 
     public function readProtocols()
@@ -57,7 +145,9 @@ class ProtocolController extends Controller
 
         foreach ($protocols as $protocol) {
             $protocolData = $protocol->toArray();
-            unset($protocolData['protocol_id']);
+            unset($protocolData['id']);
+            unset($protocolData['keywords']);
+            unset($protocolData['pdf']);
             unset($protocolData['updated_at']);
             unset($protocolData['created_at']);
             $formattedProtocols[] = $protocolData;
@@ -99,11 +189,7 @@ class ProtocolController extends Controller
             return response()->json(['message' => 'Protocolo no encontrado'], 404);
         }
 
-
-        $user = User::find($protocol->protocol_id);
-        $user->delete();
         $protocol->delete();
         return response()->json(['message' => 'Protocolo eliminado exitosamente'], 200);
     }
-
 }
