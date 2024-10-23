66c72d16ae1a16f3251ef5ad8bd9f90e73312eca
Francisco <franjav.cast@gmail.com>
2024-10-22 21:27:04 -0600

    CorreosConfig

--- src/routes/api.php
+++ src/routes/api.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\StaffController;
 use App\Http\Controllers\ProtocolController;
 use App\Http\Controllers\UsersController;
 use App\Http\Controllers\DatesAndTermsController;
+use App\Mail\EnvioCorreoMailabre;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -54,8 +56,17 @@ Route::post('/dates', [DatesAndTermsController::class, 'createSchoolCycle']);
 Route::get('/dates', [DatesAndTermsController::class, 'getSchoolCycle']);
 Route::get('/dates/all', [DatesAndTermsController::class, 'getAllSchoolCycles']);
 
+// Email routes
+Route::get('/correo', function() {
+    Mail::to('franjav.cast@gmail.com')
+        ->send(new EnvioCorreoMailabre);
+    return "Mensaje Enviado";
+})->name('api.correo');
+
 // Protected routes
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::post('/keepalive', [AuthController::class, 'keepAlive']);
 });
+
+
