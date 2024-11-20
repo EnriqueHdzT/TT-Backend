d91a8380841114db06c7394d7db396b1d7c7036a
franjavcast <116543130+franjavcast@users.noreply.github.com>
2024-11-19 22:44:22 -0600

    File service

---
+++ src/app/Services/FileService.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Response;
+
+class FileService
+{
+    public function getFile($_filename)
+    {
+        // Clean the file name
+        $filename = str_replace('../', '', $_filename);
+
+        // Adjust this path to match the documents' location
+        $path = storage_path('app/uploads/' . $filename);
+
+        if (!File::exists($path)) {
+            return response()->json(['error' => 'File not found.'], 404);
+        }
+
+        $file = File::get($path);
+        $type = File::mimeType($path);
+
+        return Response::make($file, 200)->header("Content-Type", $type);
+    }
+}
